4d051fa3e2838189e70b0132099210e9831469cd
Misha <mishakl10@ukr.net>
2021-03-14 20:52:04 +0200

    Connect with Cloudinary

--- routes/category.js
+++ routes/category.js
@@ -3,12 +3,13 @@ const router = express.Router()
 
 const { authCheck, adminCheck } = require('../middlewares/auth')
 
-const { create, read, update, remove, list } = require('../controllers/category')
+const { create, read, update, remove, list, getSubs } = require('../controllers/category')
 
 router.post('/category', authCheck, adminCheck, create)
 
 router.get('/categories', list)
 router.get('/category/:slug', read)
+router.get('/category/subs/:_id', getSubs)
 
 router.put('/category/:slug', authCheck, adminCheck, update)
 router.delete('/category/:slug', authCheck, adminCheck, remove)
