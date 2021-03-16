d8f2f42e13ee6bfa2f52d0b0bf65c3ce6f80a163
Misha <mishakl10@ukr.net>
2021-03-16 21:02:34 +0200

    Home page: Pagination

--- routes/product.js
+++ routes/product.js
@@ -3,13 +3,16 @@ const router = express.Router()
 
 const { authCheck, adminCheck } = require('../middlewares/auth')
 
-const { create, listAll, remove, read, update } = require('../controllers/product')
+const { create, listAll, remove, read, update, list, productsCount } = require('../controllers/product')
 
 router.post('/product', authCheck, adminCheck, create)
+router.post('/products', list)
+
+router.get('/products/total', productsCount)
 router.get('/products/:count', listAll)
+router.get('/product/:slug', read)
 
 router.delete('/products/:slug', authCheck, adminCheck, remove)
-router.get('/product/:slug', read)
 
 router.put('/product/:slug', authCheck, adminCheck, update)
 
