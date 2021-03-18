37adecd5ffd4c6a4473188655956343027a00989
Misha <mishakl10@ukr.net>
2021-03-18 21:55:19 +0200

    Search by color, brand, shipping

--- controllers/product.js
+++ controllers/product.js
@@ -204,11 +204,37 @@ const handleSub = async (req, res, sub) => {
   res.json(products)
 }
 
+const handleShipping = async (req, res, shipping) => {
+  const products = await Product.find({ shipping })
+    .populate('category', '_id name')
+    .populate('subs', '_id name')
+    .exec()
+
+  res.json(products)
+}
+
+const handleColor = async (req, res, color) => {
+  const products = await Product.find({ color })
+    .populate('category', '_id name')
+    .populate('subs', '_id name')
+    .exec()
+
+  res.json(products)
+}
+
+const handleBrand = async (req, res, brand) => {
+  const products = await Product.find({ brand })
+    .populate('category', '_id name')
+    .populate('subs', '_id name')
+    .exec()
+
+  res.json(products)
+}
+
 exports.searchFilters = async (req, res) => {
-  const { query, price, category, stars, sub } = req.body
+  const { query, price, category, stars, sub, shipping, color, brand } = req.body
 
   if (query) {
-    console.log(query)
     await handleQuery(req, res, query)
   }
 
@@ -227,5 +253,18 @@ exports.searchFilters = async (req, res) => {
   if (sub) {
     await handleSub(req, res, sub)
   }
+
+  if (shipping) {
+    await handleShipping(req, res, shipping)
+  }
+
+  if (color) {
+    await handleColor(req, res, color)
+  }
+
+  if (brand) {
+    await handleBrand(req, res, brand)
+  }
+
 
 }
