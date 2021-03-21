80218e091d6e400e641c3ce5481e7812916e9553
Misha <mishakl10@ukr.net>
2021-03-21 18:37:09 +0200

    Get Cart by id

--- controllers/user.js
+++ controllers/user.js
@@ -39,3 +39,14 @@ exports.userCart = async (req, res) => {
   console.log(newCart)
   res.json({ ok: true })
 }
+
+exports.getUserCart = async (req, res) => {
+  const user = await User.findOne({ email: req.user.email }).exec()
+
+  let cart = await Cart.findOne({ orderedBy: user._id})
+    .populate('products.product')
+    .exec()
+
+  const { products, cartTotal, totalAfterDiscount } = cart
+  res.json({ products, cartTotal, totalAfterDiscount })
+}
