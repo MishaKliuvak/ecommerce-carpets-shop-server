32b9a64b1a26d9b6bce9fec1719932594e17909b
Misha <mishakl10@ukr.net>
2021-03-22 20:20:02 +0200

    Payment with coupons

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -5,17 +5,25 @@ const Coupon = require('../models/coupon')
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 exports.createPaymentIntent = async (req, res) => {
+  //console.log(req.body.couponApplied)
+  const { couponApplied } = req.body
   const user = await User.findOne({ email: req.user.email}).exec()
 
-  const { cartTotal } = await Cart.findOne({ orderedBy: user._id}).exec()
+  const { cartTotal, totalAfterDiscount } = await Cart.findOne({ orderedBy: user._id}).exec()
+  let finalAmount = cartTotal * 100
 
-  console.log(cartTotal)
+  if (couponApplied && totalAfterDiscount)
+    finalAmount = totalAfterDiscount * 100
+
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: cartTotal * 100,
+    amount: finalAmount,
     currency: "usd",
   });
 
   res.send({
     clientSecret: paymentIntent.client_secret,
+    cartTotal,
+    totalAfterDiscount,
+    payable: finalAmount
   });
 };
