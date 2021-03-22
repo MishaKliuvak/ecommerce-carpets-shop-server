e5f2366dcff1ab6e09b2ceef197455c01d728481
Misha <mishakl10@ukr.net>
2021-03-22 11:09:03 +0200

    Save the user address

--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const { authCheck } = require('../middlewares/auth')
-const { userCart, getUserCart, emptyCart } = require('../controllers/user')
+const { userCart, getUserCart, emptyCart, saveAddress } = require('../controllers/user')
 
 router.post('/user/cart', authCheck, userCart)
+router.post('/user/address', authCheck, saveAddress)
 router.get('/user/cart', authCheck, getUserCart)
 router.delete('/user/cart', authCheck, emptyCart)
 
