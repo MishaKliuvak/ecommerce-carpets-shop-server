2cc8376502b12a253656390085d5156bbf70647e
Misha <mishakl10@ukr.net>
2021-03-22 16:43:31 +0200

    Check the coupon

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 const User = require('../models/user')
 const Product = require('../models/product')
 const Cart = require('../models/cart')
+const Coupon = require('../models/coupon')
 
 exports.userCart = async (req, res) => {
   const { cart } = req.body
@@ -65,4 +66,26 @@ exports.saveAddress = async (req, res) => {
     .exec()
 
   res.json({ ok: 'true' })
+}
+
+exports.applyCoupon = async (req, res) => {
+  const { coupon } = req.body
+
+  const validCoupon = await Coupon.findOne({ name: coupon.name }).exec()
+  if (validCoupon === null) {
+    return res.json({
+      err: 'Not found coupon'
+    })
+  }
+
+  const user = await User.findOne({ email: req.user.email }).exec()
+  let { products, cartTotal } = await Cart.findOne({ orderedBy: user._id})
+    .populate('products.product', '_id title price')
+    .exec()
+
+  let totalAfterDiscount = (cartTotal - (cartTotal * validCoupon.discount / 100)).toFixed(2)
+
+  await Cart.findOneAndUpdate({ orderedBy: user._id }, { totalAfterDiscount }, { new: true }).exec()
+
+  res.json(totalAfterDiscount)
 }
--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const { authCheck } = require('../middlewares/auth')
-const { userCart, getUserCart, emptyCart, saveAddress } = require('../controllers/user')
+const { userCart, getUserCart, emptyCart, saveAddress, applyCoupon } = require('../controllers/user')
 
 router.post('/user/cart', authCheck, userCart)
 router.post('/user/address', authCheck, saveAddress)
+router.post('/user/cart/coupon', authCheck, applyCoupon)
 router.get('/user/cart', authCheck, getUserCart)
 router.delete('/user/cart', authCheck, emptyCart)
 
