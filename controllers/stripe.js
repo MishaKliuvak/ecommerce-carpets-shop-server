790eba15dfc1bb8a34caa22e2ee726ba1bcb318a
Misha <mishakl10@ukr.net>
2021-03-22 20:04:46 +0200

    Change the amount of the payment

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -2,15 +2,20 @@ const User = require('../models/user')
 const Cart = require('../models/cart')
 const Product = require('../models/product')
 const Coupon = require('../models/coupon')
-const stripe = require('stripe')(process.env.STRIPE_SECRET)
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 exports.createPaymentIntent = async (req, res) => {
-  const paymentIntent = await stripe.paymentIntent.create({
-    amount: 100,
-    currency: 'usd',
-  })
+  const user = await User.findOne({ email: req.user.email}).exec()
+
+  const { cartTotal } = await Cart.findOne({ orderedBy: user._id}).exec()
+
+  console.log(cartTotal)
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: cartTotal * 100,
+    currency: "usd",
+  });
 
   res.send({
-    clientSecret: paymentIntent.client_secret
-  })
-}
+    clientSecret: paymentIntent.client_secret,
+  });
+};
