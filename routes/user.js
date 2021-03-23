836ef0a63453ac1c1eccef65349e6f860eab1b78
Misha <mishakl10@ukr.net>
2021-03-23 18:18:56 +0200

    Create order controller

--- routes/user.js
+++ routes/user.js
@@ -2,12 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 const { authCheck } = require('../middlewares/auth')
-const { userCart, getUserCart, emptyCart, saveAddress, applyCoupon } = require('../controllers/user')
+const { userCart, getUserCart, emptyCart, saveAddress, applyCoupon, createOrder } = require('../controllers/user')
 
 router.post('/user/cart', authCheck, userCart)
 router.post('/user/address', authCheck, saveAddress)
 router.post('/user/cart/coupon', authCheck, applyCoupon)
+router.post('/user/order', authCheck, createOrder)
+
 router.get('/user/cart', authCheck, getUserCart)
+
 router.delete('/user/cart', authCheck, emptyCart)
 
 module.exports = router
