9d69961e9b4d6585b7b87879f1734c1c6f5bc278
Misha <mishakl10@ukr.net>
2021-03-23 19:37:18 +0200

    Get all orders by id

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const { authCheck } = require('../middlewares/auth')
-const { userCart, getUserCart, emptyCart, saveAddress, applyCoupon, createOrder } = require('../controllers/user')
+const { userCart, getUserCart, emptyCart, saveAddress, applyCoupon, createOrder, orders } = require('../controllers/user')
 
 router.post('/user/cart', authCheck, userCart)
 router.post('/user/address', authCheck, saveAddress)
@@ -10,6 +10,7 @@ router.post('/user/cart/coupon', authCheck, applyCoupon)
 router.post('/user/order', authCheck, createOrder)
 
 router.get('/user/cart', authCheck, getUserCart)
+router.get('/user/orders', authCheck, orders)
 
 router.delete('/user/cart', authCheck, emptyCart)
 
